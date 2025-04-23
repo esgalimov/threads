8826c80d474f76bdcfd84c72e8658a53e2ee2b38
esgalimov <esgalimov@gmail.com>
2025-04-23 15:04:13 +0300

    sort on each thread, need to merge

--- sort.cc
+++ sort.cc
@@ -3,38 +3,59 @@
 
 constexpr int NUM_THREADS = 4;
 
+static_assert(!(NUM_THREADS == 0) && !(NUM_THREADS & (NUM_THREADS - 1)), "NUM_THREADS must be power of 2 ");
+
 int main(int argc, char* argv[]) {
-    std::cout << "HELLO" << std::endl;
+    std::vector<int> vec{4, 5, 1, 2, 3, 4, 3, 1, 4};
 
-    std::vector<int> vec{4, 5, 1, 2, 3};
+    #ifdef DEBUG
+        for (auto& elem : vec) std::cout << elem << " ";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    #endif
 
-    parallel_sorter::sort_pull_t sp;
+    parallel_sorter::sort_pull_t sp[NUM_THREADS];
+    int thr_vec_size = vec.size() / NUM_THREADS;
+    auto it = vec.begin();
 
-    sp.begin = vec.begin();
-    sp.end = vec.end();
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        sp[i].begin = it;
 
-    parallel_sorter::thread_sort(&sp);
+        if (i != NUM_THREADS - 1) {
+            it += thr_vec_size;
+            sp[i].end = it;
+        } else sp[i].end = vec.end();
+    } 
 
-    for (auto& elem : vec) std::cout << elem << std::endl;
+    #ifdef DEBUG
+        for (int i = 0; i < NUM_THREADS; ++i) {
+            for (auto it = sp[i].begin; it != sp[i].end; ++it) {
+                std::cout << *it << " ";
+            }
+            std::cout << std::endl;
+        }
+    #endif
+    
+    pthread_t thr[NUM_THREADS];
 
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        int ret_code = pthread_create(&thr[i], NULL, parallel_sorter::thread_sort, &sp[i]);
 
+        if (ret_code) {
+            std::cerr << "error: pthread_create, ret_code: " << ret_code << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
 
-    // pthread_t thr[NUM_THREADS]; int i, rc;
-    // thread_data_t thr_data[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        std::cout << thr[i] << std::endl;
+        pthread_join(thr[i], NULL);
+    }
 
-    // for(i = 0; i< NUM_THREADS; ++i) {
-    //     thr_data[i].tid = i;
-    //     if((rc = pthread_create(&thr[i], NULL, thr_func, &thr_data[i]))) {
-    //         fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
-    //         return EXIT_FAILURE;
-    //     }
-    // }
-    //     for(i = 0; i< NUM_THREADS; ++i) {
-    //     pthread_join(thr[i], NULL);
-    // }
-    // return EXIT_SUCCESS;
+    #ifdef DEBUG
+        for (auto& elem : vec) std::cout << elem << " ";
+        std::cout << std::endl;
+    #endif
 
-
-
-    return 0;
+    return EXIT_SUCCESS;
 }
--- sort.hh
+++ sort.hh
@@ -7,22 +7,39 @@
 
 namespace parallel_sorter {
 
+using vec_it_t = typename std::vector<int>::iterator;
+
 struct sort_pull_t final {
-    using vec_it_t = typename std::vector<int>::iterator;
     vec_it_t begin;
     vec_it_t end;
+};
+
+struct merge_pull_t final {
+    vec_it_t src_begin1;
+    vec_it_t src_end1;
+
+    vec_it_t src_begin2;
+    vec_it_t src_end2;
+
+    vec_it_t dst_begin;
+    vec_it_t dst_end;
 };
 
 
 void* thread_sort(void* arg) {
     sort_pull_t* data = static_cast<sort_pull_t*>(arg);
 
-    for (auto it = data->begin; it != data->end; ++it) {
-        std::cout << *it << std::endl;
-    }
-    std::cout << std::endl;
+    // for (auto it = data->begin; it != data->end; ++it) {
+    //     std::cout << *it << std::endl;
+    // }
+    // std::cout << std::endl;
 
     std::sort(data->begin, data->end);
+    pthread_exit(NULL);
+}
+
+void* thread_merge(void* arg) {
+    pthread_exit(NULL);
 }
 
 }; // <-- namespace parallel_sorter
