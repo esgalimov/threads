3e3d3de7abc2dcc351dde2aed5d3ec33f51151d6
esgalimov <esgalimov@gmail.com>
2025-04-27 23:26:16 +0300

    all tests

--- sort_test.cc
+++ sort_test.cc
@@ -11,6 +11,9 @@ using namespace parallel_sorter;
 
 std::ofstream out("test_res.txt");
 
+constexpr int WARMUP_ITERS = 10;
+constexpr int TEST_ITERS   = 20;
+
 std::vector<int> gen_data(long elem_num) {
     std::vector<int> vec(elem_num);
 
@@ -25,29 +28,8 @@ std::vector<int> gen_data(long elem_num) {
     return vec;
 }
 
-bool param_test(long thr_num, long elem_num, std::ostream& os) {
-    std::vector<int> vec = gen_data(elem_num);
-
-    os << "THR_NUM = " << thr_num << std::endl << "ELEM_NUM = " << elem_num << std::endl;
-
-    const int WARMUP_ITERS = 10;
-    const int TEST_ITERS   = 20;
-    double tm = 0.0;
-
-    // =========== PARRALEL SORT =================
-    for (int i = 0; i < WARMUP_ITERS; ++i) { // warmup
-        std::vector<int> vec_warmup(vec);
-        parallel_sorter::merge_sort(vec_warmup, thr_num);
-    }
-
-    for (int i = 0; i < TEST_ITERS; ++i) { // benchmark
-        std::vector<int> vec_test(vec);
-        auto start = std::chrono::high_resolution_clock::now();
-        parallel_sorter::merge_sort(vec_test, thr_num);
-        tm += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
-    }
-    os << "PARRALEL SORT = " << tm / TEST_ITERS << " us" << std::endl;
-    //=============================================
+void param_test_std(std::vector<int>& vec, std::ostream& os) {
+    os << "THR_NUM = 1" << std::endl << "ELEM_NUM = " << vec.size() << std::endl;
 
     // =============== STD::SORT ==================
     for (int i = 0; i < WARMUP_ITERS; ++i) { // warmup
@@ -55,7 +37,7 @@ bool param_test(long thr_num, long elem_num, std::ostream& os) {
         std::sort(vec_warmup.begin(), vec_warmup.end());
     }
 
-    tm = 0.0;
+    double tm = 0.0;
     for (int i = 0; i < TEST_ITERS; ++i) { // benchmark
         std::vector<int> vec_test(vec);
         auto start = std::chrono::high_resolution_clock::now();
@@ -92,6 +74,27 @@ bool param_test(long thr_num, long elem_num, std::ostream& os) {
     }
     os << "STD::QSORT SORT = " << tm / TEST_ITERS  << " us" << std::endl << std::endl;
     //=============================================
+}
+
+bool param_test_parallel(std::vector<int>& vec, long thr_num, std::ostream& os) {
+    os << "THR_NUM = " << thr_num << std::endl << "ELEM_NUM = " << vec.size() << std::endl;
+
+    double tm = 0.0;
+
+    // =========== PARRALEL SORT =================
+    for (int i = 0; i < WARMUP_ITERS; ++i) { // warmup
+        std::vector<int> vec_warmup(vec);
+        parallel_sorter::merge_sort(vec_warmup, thr_num);
+    }
+
+    for (int i = 0; i < TEST_ITERS; ++i) { // benchmark
+        std::vector<int> vec_test(vec);
+        auto start = std::chrono::high_resolution_clock::now();
+        parallel_sorter::merge_sort(vec_test, thr_num);
+        tm += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    }
+    os << "PARRALEL SORT = " << tm / TEST_ITERS << " us" << std::endl << std::endl;
+    //=============================================
 
     std::vector<int> vec2(vec);
     std::sort(vec2.begin(), vec2.end());
@@ -106,15 +109,68 @@ bool param_test(long thr_num, long elem_num, std::ostream& os) {
 
 }
 
+constexpr int MAX_POW = 15;
+
+TEST(std_sort_qsort, threads_1) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        param_test_std(vec, out);
+    }
+}
+
 TEST(merge_sort, threads_2) {
-    for (int i = 0; i <= 15; ++i) {
-        ASSERT_TRUE(param_test(2, 1000 * std::pow(2, i), out));
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 2, out));
     }
 }
 
 TEST(merge_sort, threads_4) {
-    for (int i = 0; i <= 15; ++i) {
-        ASSERT_TRUE(param_test(4, 1000 * std::pow(2, i), out));
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 4, out));
+    }
+}
+
+TEST(merge_sort, threads_8) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 8, out));
+    }
+}
+
+TEST(merge_sort, threads_16) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 16, out));
+    }
+}
+
+TEST(merge_sort, threads_32) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 32, out));
+    }
+}
+
+TEST(merge_sort, threads_64) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 64, out));
+    }
+}
+
+TEST(merge_sort, threads_128) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 128, out));
+    }
+}
+
+TEST(merge_sort, threads_256) {
+    for (int i = 0; i <= MAX_POW; ++i) {
+        std::vector<int> vec = gen_data(1000 * std::pow(2, i));
+        ASSERT_TRUE(param_test_parallel(vec, 256, out));
     }
 }
 
